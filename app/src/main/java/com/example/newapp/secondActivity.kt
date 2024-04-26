886291206851d39a5b2e80b36585c384b5c75572
G3nKai <kulikovila2005@gmail.com>
2024-04-26 18:59:18 +0700

    добавлена камера, снимок передается на третье активити

--- app/src/main/java/com/example/newapp/secondActivity.kt
+++ app/src/main/java/com/example/newapp/secondActivity.kt
@@ -2,17 +2,23 @@ package com.example.newapp
 
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.io.ByteArrayOutputStream
 
 class secondActivity : AppCompatActivity() {
     companion object {
         private const val GALLERY_REQUEST_CODE = 1
         private const val CAMERA_REQUEST_CODE = 2
+        private const val CAMERA_PERMISSION_CODE = 3
     }
     private lateinit var imageView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,11 +28,30 @@ class secondActivity : AppCompatActivity() {
 
         imageView = findViewById(R.id.imageViewer) // Инициализация imageView
         val pickImageButton: Button = findViewById(R.id.button_gallery)
+        val takePhotoButton: Button = findViewById(R.id.button_photo)
 
         pickImageButton.setOnClickListener {
             dispatchPickImageIntent()
         }
+
+        takePhotoButton.setOnClickListener {
+            dispatchTakePhotoIntent()
+        }
     }
+
+    private fun dispatchTakePhotoIntent() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE)
+        }
+        else {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(android.Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE)
+        }
+    }
+
 
 
     private fun dispatchPickImageIntent() {
@@ -41,10 +66,22 @@ class secondActivity : AppCompatActivity() {
             val imageUri = data?.data
             imageView.setImageURI(imageUri)
             val intent = Intent(this, thirdActivity::class.java)
+            intent.putExtra("imageSource", "gallery")
             intent.putExtra("imageUri", imageUri)
             startActivity(intent)
+        }
+        else if(requestCode == CAMERA_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val photo: Bitmap = data!!.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(photo)
 
+            val byteArrayOutputStream = ByteArrayOutputStream()
+            photo.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream)
+            val byteArray: ByteArray = byteArrayOutputStream.toByteArray()
 
+            val intent = Intent(this, thirdActivity::class.java)
+            intent.putExtra("imageSource", "camera")
+            intent.putExtra("imageByteArray", byteArray)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/newapp/thirdActivity.kt
+++ app/src/main/java/com/example/newapp/thirdActivity.kt
@@ -1,5 +1,6 @@
 package com.example.newapp
 
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
@@ -9,8 +10,17 @@ class thirdActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
-        val imageUri: Uri? = intent.getParcelableExtra("imageUri")
-        findViewById<ImageView>(R.id.imageView3).setImageURI(imageUri)
 
+        val imageSource = intent.getStringExtra("imageSource")
+        if (imageSource == "gallery") {
+            val imageUri: Uri? = intent.getParcelableExtra("imageUri")
+            findViewById<ImageView>(R.id.imageView3).setImageURI(imageUri)
+        }
+        else if (imageSource == "camera") {
+            val byteArray = intent.getByteArrayExtra("imageByteArray")
+            val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray!!.size)
+
+            findViewById<ImageView>(R.id.imageView3).setImageBitmap(bitmap)
+        }
     }
 }
