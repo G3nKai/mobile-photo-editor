5bfd2d24b0029f6b53015d021b61e3882d12c5a7
ruvich <alaptev1308@gmail.com>
2024-05-07 15:29:58 +0700

    начальный дизайн

--- app/src/main/java/com/example/newapp/FiltersActivity.kt
+++ app/src/main/java/com/example/newapp/FiltersActivity.kt
@@ -5,18 +5,27 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.newapp.databinding.ActivityFiltersBinding
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
+import kotlin.math.atan2
+import kotlin.math.min
+import kotlin.math.sin
+import kotlin.math.sqrt
 
 class FiltersActivity: AppCompatActivity() {
     private lateinit var binding: ActivityFiltersBinding
+    private lateinit var originalBitmap: Bitmap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFiltersBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val byteArray = intent.getByteArrayExtra("imageByteArray")
-        val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray!!.size)
+        val imageUri = Uri.parse(intent.getStringExtra("imageUri"))
+        binding.imageViewFilter.setImageURI(imageUri)
 
-        binding.imageView2.setImageBitmap(bitmap)
+        originalBitmap = BitmapFactory.decodeStream(contentResolver.openInputStream(imageUri))
+        binding.imageViewFilter.setImageBitmap(originalBitmap)
     }
 }
