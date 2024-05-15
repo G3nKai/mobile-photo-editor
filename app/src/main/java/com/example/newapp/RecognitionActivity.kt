762e7445a8afe7b7d4283fe28875e66aa87c7131
Guts <komroncurabekov645@gmail.com>
2024-05-15 11:23:41 +0700

    модифицировал код

--- app/src/main/java/com/example/newapp/RecognitionActivity.kt
+++ app/src/main/java/com/example/newapp/RecognitionActivity.kt
@@ -1,27 +1,22 @@
 package com.example.newapp
 
-import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.newapp.databinding.ActivityRecognitionBinding
-import com.example.newapp.databinding.ActivityRotateBinding
 
 class RecognitionActivity: AppCompatActivity() {
-    private lateinit var binding: ActivityRotateBinding
-    private lateinit var originalBitmap: Bitmap
+    private lateinit var binding: ActivityRecognitionBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityRotateBinding.inflate(layoutInflater)
+        binding = ActivityRecognitionBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val imageUri = Uri.parse(intent.getStringExtra("imageUri"))
-        binding.imageView2.setImageURI(imageUri)
-        originalBitmap = BitmapFactory.decodeStream(contentResolver.openInputStream(imageUri))
-        binding.imageView2.setImageBitmap(originalBitmap)
+        val byteArray = intent.getByteArrayExtra("imageByteArray")
+        val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray!!.size)
 
-        
+        binding.imageView2.setImageBitmap(bitmap)
     }
 }
