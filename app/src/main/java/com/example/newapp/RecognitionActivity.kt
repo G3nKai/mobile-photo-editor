46e0ecb760bf3018c42e814c81db62099251c118
Guts <komroncurabekov645@gmail.com>
2024-05-15 16:22:03 +0700

    сырая сегментация

--- app/src/main/java/com/example/newapp/RecognitionActivity.kt
+++ app/src/main/java/com/example/newapp/RecognitionActivity.kt
@@ -1,22 +1,80 @@
 package com.example.newapp
 
+import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.newapp.databinding.ActivityRecognitionBinding
+import org.opencv.android.OpenCVLoader
+import org.opencv.android.Utils
+import org.opencv.core.Core
+import org.opencv.core.Mat
+import org.opencv.core.Scalar
+import org.opencv.imgproc.Imgproc
+import org.opencv.objdetect.CascadeClassifier
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
 
-class RecognitionActivity: AppCompatActivity() {
+class RecognitionActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRecognitionBinding
+    private lateinit var originalBitmap: Bitmap
+    private lateinit var mRgb: Mat
+    private lateinit var mGray: Mat
+
+    private lateinit var cascadeClassifier: CascadeClassifier
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRecognitionBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val byteArray = intent.getByteArrayExtra("imageByteArray")
-        val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray!!.size)
+        if (!OpenCVLoader.initDebug()) {
+            Log.e("OpenCV", "Unable to load OpenCV")
+        } else {
+            Log.d("OpenCV", "OpenCV loaded successfully")
+        }
 
-        binding.imageView2.setImageBitmap(bitmap)
+        val imageUri = Uri.parse(intent.getStringExtra("imageUri"))
+        binding.imageView2.setImageURI(imageUri)
+        originalBitmap = BitmapFactory.decodeStream(contentResolver.openInputStream(imageUri))
+        binding.imageView2.setImageBitmap(originalBitmap)
+
+        // Initialize Mat objects
+        mRgb = Mat()
+        mGray = Mat()
+
+        Utils.bitmapToMat(originalBitmap, mRgb)
+        Imgproc.cvtColor(mRgb, mGray, Imgproc.COLOR_BGR2GRAY)
+
+        val modifiedBitmap = Bitmap.createBitmap(mGray.cols(), mGray.rows(), Bitmap.Config.ARGB_8888)
+        Utils.matToBitmap(mGray, modifiedBitmap)
+
+        binding.imageView2.setImageBitmap(modifiedBitmap)
+
+        // Loading the model
+        try {
+            // Copy the resource into a temp file so OpenCV can load it
+            val inputStream = resources.openRawResource(R.raw.haarcascade_frontalface_default)
+            val cascadeDir = getDir("cascade", Context.MODE_PRIVATE)
+            val cascadeFile = File(cascadeDir, "haarcascade_frontalface_default.xml")
+            val outputStream = FileOutputStream(cascadeFile)
+
+            val buffer = ByteArray(4096)
+            var bytesRead: Int
+            while (inputStream.read(buffer).also { bytesRead = it } != -1) {
+                outputStream.write(buffer, 0, bytesRead)
+            }
+            inputStream.close()
+            outputStream.close()
+
+            // Load the cascade classifier
+            cascadeClassifier = CascadeClassifier(cascadeFile.absolutePath)
+        } catch (e: Exception) {
+            Log.e("OpenCVActivity", "Error loading cascade", e)
+        }
     }
 }
