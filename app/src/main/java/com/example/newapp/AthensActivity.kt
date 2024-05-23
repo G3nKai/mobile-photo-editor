19acba39bc5b856017d148a32e0161d62847ae67
G3nKai <kulikovila2005@gmail.com>
2024-05-23 22:26:29 +0700

    база для афинных в kt файле

--- app/src/main/java/com/example/newapp/AthensActivity.kt
+++ app/src/main/java/com/example/newapp/AthensActivity.kt
@@ -1,5 +1,6 @@
 package com.example.newapp
 
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
@@ -8,15 +9,20 @@ import com.example.newapp.databinding.ActivityAthensBinding
 
 class AthensActivity: AppCompatActivity() {
     private lateinit var binding: ActivityAthensBinding
+    private lateinit var originalBitmap: Bitmap
+    private lateinit var backUpBitmap: Bitmap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAthensBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val byteArray = intent.getByteArrayExtra("imageByteArray")
-        val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray!!.size)
+        val imageUri = Uri.parse(intent.getStringExtra("imageUri"))
+        binding.imageView2.setImageURI(imageUri)
 
-        binding.imageView2.setImageBitmap(bitmap)
+        originalBitmap = BitmapFactory.decodeStream(contentResolver.openInputStream(imageUri))
+        binding.imageView2.setImageBitmap(originalBitmap)
+
+        backUpBitmap = originalBitmap.copy(originalBitmap.config, true)
     }
 }
