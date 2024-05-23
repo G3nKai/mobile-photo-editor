e93796bea03850f3f1eedf12473f1a9b8fd5e802
G3nKai <kulikovila2005@gmail.com>
2024-05-23 23:53:46 +0700

    отправка и отмена фотки

--- app/src/main/java/com/example/newapp/AthensActivity.kt
+++ app/src/main/java/com/example/newapp/AthensActivity.kt
@@ -1,11 +1,16 @@
 package com.example.newapp
 
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.media.MediaScannerConnection
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import androidx.appcompat.app.AppCompatActivity
 import com.example.newapp.databinding.ActivityAthensBinding
+import java.io.File
+import java.io.FileOutputStream
 
 class AthensActivity: AppCompatActivity() {
     private lateinit var binding: ActivityAthensBinding
@@ -24,5 +29,32 @@ class AthensActivity: AppCompatActivity() {
         binding.imageView2.setImageBitmap(originalBitmap)
 
         backUpBitmap = originalBitmap.copy(originalBitmap.config, true)
+
+        binding.accept.setOnClickListener {
+            val scaledUri = dispatchToGallery(originalBitmap)
+
+            val intent = Intent(this, ThirdActivity::class.java)
+            intent.putExtra("imageSource", "gallery")
+            intent.putExtra("imageUri", scaledUri.toString())
+            startActivity(intent)
+        }
+
+        binding.cancel.setOnClickListener {
+            originalBitmap = backUpBitmap
+            binding.imageView2.setImageBitmap(originalBitmap)
+        }
+    }
+
+    private fun dispatchToGallery(bitmap: Bitmap): Uri {
+        val imagesDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val imageFile = File(imagesDir, "scaled_image.png")
+
+        val outputStream = FileOutputStream(imageFile)
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+        outputStream.close()
+
+        MediaScannerConnection.scanFile(this, arrayOf(imageFile.absolutePath), null, null)
+
+        return Uri.fromFile(imageFile)
     }
 }
