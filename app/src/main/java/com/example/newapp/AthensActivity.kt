73957fad3dee8b3f7782b761589e53541c8ae239
G3nKai <kulikovila2005@gmail.com>
2024-05-25 10:08:33 +0700

    добавлены афинные преобразования. Скейлинг скопирован из ScalingActivity

--- app/src/main/java/com/example/newapp/AthensActivity.kt
+++ app/src/main/java/com/example/newapp/AthensActivity.kt
@@ -13,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.newapp.databinding.ActivityAthensBinding
 import java.io.File
 import java.io.FileOutputStream
+import kotlin.math.roundToInt
 
 class AthensActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAthensBinding
@@ -43,7 +44,7 @@ class AthensActivity : AppCompatActivity() {
         binding.imageViewAthens.setImageBitmap(mutableBitmap)
 
         binding.textViewSave.setOnClickListener {
-            val scaledUri = dispatchToGallery(originalBitmap)
+            val scaledUri = dispatchToGallery(mutableBitmap)
 
             val intent = Intent(this, ThirdActivity::class.java)
             intent.putExtra("imageSource", "gallery")
@@ -74,6 +75,12 @@ class AthensActivity : AppCompatActivity() {
             binding.imageViewAthens.setImageBitmap(mutableBitmap)
             points = arrayOfNulls(3)
             currentPointIndex = 0
+
+            if (pointsStart.any { it == null }) {
+                Toast.makeText(this, "Start не до конца заполнен.", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Start заполнен корректно.", Toast.LENGTH_SHORT).show()
+            }
         }
 
         binding.athenesEnd.setOnClickListener {
@@ -83,9 +90,13 @@ class AthensActivity : AppCompatActivity() {
                 pointsEnd = points.copyOf()
                 mutableBitmap = originalBitmap.copy(Bitmap.Config.ARGB_8888, true)
                 binding.imageViewAthens.setImageBitmap(mutableBitmap)
+                Toast.makeText(this, "End заполнен корректно.", Toast.LENGTH_SHORT).show()
             }
         }
 
+        binding.athenesButton.setOnClickListener {
+            performAthenesTransformation()
+        }
     }
 
     private fun touchPoint(event: MotionEvent) {
@@ -125,6 +136,167 @@ class AthensActivity : AppCompatActivity() {
         canvas.drawCircle(x, y, 20f, paint)
         binding.imageViewAthens.invalidate()
     }
+
+    private fun performAthenesTransformation() {
+        if (pointsStart.any { it == null } || pointsEnd.any { it == null }) {
+            Toast.makeText(this, "Заполните Start и End", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val matrix = Matrix()
+        val src = floatArrayOf(
+            pointsStart[0]!!.x, pointsStart[0]!!.y,
+            pointsStart[1]!!.x, pointsStart[1]!!.y,
+            pointsStart[2]!!.x, pointsStart[2]!!.y
+        )
+        val dst = floatArrayOf(
+            pointsEnd[0]!!.x, pointsEnd[0]!!.y,
+            pointsEnd[1]!!.x, pointsEnd[1]!!.y,
+            pointsEnd[2]!!.x, pointsEnd[2]!!.y
+        )
+        matrix.setPolyToPoly(src, 0, dst, 0, 3)
+
+        val newWidth = originalBitmap.width
+        val newHeight = originalBitmap.height
+        val transformedBitmap = Bitmap.createBitmap(newWidth, newHeight, Bitmap.Config.ARGB_8888)
+
+        val canvas = Canvas(transformedBitmap)
+
+        val scaleFactorX = originalBitmap.width.toFloat() / transformedBitmap.width.toFloat()
+        val scaleFactorY = originalBitmap.height.toFloat() / transformedBitmap.height.toFloat()
+
+
+        if (scaleFactorX > 1 && scaleFactorY > 1) {
+            val scaleFactor = maxOf(scaleFactorX, scaleFactorY)
+            val newBitmap = bilinear(originalBitmap, scaleFactor)
+            canvas.drawBitmap(newBitmap, matrix, Paint())
+        } else {
+            val scaleFactor = minOf(scaleFactorX, scaleFactorY)
+            val newBitmap = trilinear(originalBitmap, scaleFactor)
+            canvas.drawBitmap(newBitmap, matrix, Paint())
+        }
+
+        mutableBitmap = transformedBitmap
+        binding.imageViewAthens.setImageBitmap(mutableBitmap)
+    }
+
+    private fun trilinear(bitmap: Bitmap, scaleFactor: Float): Bitmap {
+        val newWidth = (bitmap.width * scaleFactor).toInt()
+        val newHeight = (bitmap.height * scaleFactor).toInt()
+
+        val newBitmap = Bitmap.createBitmap(newWidth, newHeight, Bitmap.Config.ARGB_8888)
+
+        val scaleX = bitmap.width / newWidth.toFloat()
+        val scaleY = bitmap.height / newHeight.toFloat()
+
+        for (y in 0 until newHeight)
+            for (x in 0 until newWidth) {
+                val xOrigin = (x * scaleX).toInt()
+                val yOrigin = (y * scaleY).toInt()
+                val xFraction = x * scaleX - xOrigin
+                val yFraction = y * scaleY - yOrigin
+
+                val origin = getPixelSafely(bitmap, xOrigin, yOrigin)
+                val right = getPixelSafely(bitmap, xOrigin + 1, yOrigin)
+                val upper = getPixelSafely(bitmap, xOrigin, yOrigin + 1)
+                val upperRight = getPixelSafely(bitmap, xOrigin + 1, yOrigin + 1)
+
+                val upperLeft = getPixelSafely(bitmap, xOrigin - 1, yOrigin + 1)
+                val left = getPixelSafely(bitmap, xOrigin - 1, yOrigin)
+                val lowerLeft = getPixelSafely(bitmap, xOrigin - 1, yOrigin - 1)
+                val lower = getPixelSafely(bitmap, xOrigin, yOrigin - 1)
+
+                val zFraction = 1.0f
+
+                val interpolatedPixel = trilinearInterpolation(
+                    origin, right, upper, upperRight,
+                    upperLeft, left, lowerLeft, lower,
+                    xFraction, yFraction, zFraction
+                )
+
+                newBitmap.setPixel(x, y, interpolatedPixel)
+            }
+
+        return newBitmap
+    }
+
+    private fun trilinearInterpolation(
+        origin: Int, right: Int, upper: Int, upperRight: Int,
+        upperLeft: Int, left: Int, lowerLeft: Int, lower: Int,
+        xFraction: Float, yFraction: Float, zFraction: Float
+    ): Int {
+
+        val firstSlice =
+            bilinearInterpolation(origin, right, upper, upperRight, xFraction, yFraction)
+        val secondSlice =
+            bilinearInterpolation(upperLeft, left, lowerLeft, lower, xFraction, yFraction)
+
+        return blendColors(firstSlice, secondSlice, zFraction)
+    }
+
+    private fun getPixelSafely(bitmap: Bitmap, x: Int, y: Int): Int {
+        if (x < 0 || y < 0 || x >= bitmap.width || y >= bitmap.height)
+            return 0
+        else
+            return bitmap.getPixel(x, y)
+    }
+
+    private fun blendColors(firstSlice: Int, secondSlice: Int, fraction: Float): Int {
+        val inverseFraction = 1 - fraction
+        val alpha =
+            (Color.alpha(firstSlice) * inverseFraction + Color.alpha(secondSlice) * fraction).roundToInt()
+        val red =
+            (Color.red(firstSlice) * inverseFraction + Color.red(secondSlice) * fraction).roundToInt()
+        val green =
+            (Color.green(firstSlice) * inverseFraction + Color.green(secondSlice) * fraction).roundToInt()
+        val blue =
+            (Color.blue(firstSlice) * inverseFraction + Color.blue(secondSlice) * fraction).roundToInt()
+
+        return Color.argb(alpha, red, green, blue)
+    }
+
+    private fun bilinearInterpolation(
+        origin: Int, right: Int, upper: Int, upperRight: Int,
+        xFraction: Float, yFraction: Float
+    ): Int {
+        val firstSlice = blendColors(upper, upperRight, xFraction)
+        val secondSlice = blendColors(origin, right, xFraction)
+
+        return blendColors(firstSlice, secondSlice, yFraction)
+    }
+
+    private fun bilinear(bitmap: Bitmap, scaleFactor: Float): Bitmap {
+        val newWidth = (bitmap.width * scaleFactor).toInt()
+        val newHeight = (bitmap.height * scaleFactor).toInt()
+
+        val newBitmap = Bitmap.createBitmap(newWidth, newHeight, Bitmap.Config.ARGB_8888)
+
+        val scaleX = bitmap.width / newWidth.toFloat()
+        val scaleY = bitmap.height / newHeight.toFloat()
+
+        for (y in 0 until newHeight)
+            for (x in 0 until newWidth) {
+                val xOrigin = (x * scaleX).toInt()
+                val yOrigin = (y * scaleY).toInt()
+                val xFraction = x * scaleX - xOrigin
+                val yFraction = y * scaleY - yOrigin
+
+                val origin = getPixelSafely(bitmap, xOrigin, yOrigin)
+                val right = getPixelSafely(bitmap, xOrigin + 1, yOrigin)
+                val upper = getPixelSafely(bitmap, xOrigin, yOrigin + 1)
+                val upperRight = getPixelSafely(bitmap, xOrigin + 1, yOrigin + 1)
+
+                val interpolatedPixel = bilinearInterpolation(
+                    origin, right, upper, upperRight,
+                    xFraction, yFraction
+                )
+
+                newBitmap.setPixel(x, y, interpolatedPixel)
+            }
+
+        return newBitmap
+    }
+
 
     private fun dispatchToGallery(bitmap: Bitmap): Uri {
         val imagesDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
